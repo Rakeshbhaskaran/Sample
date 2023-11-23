0feecccd5bc0e6298bfa8242c87ab24a1aa8e686
Rakesh <rakeshbhaskaran07@gmail.com>
2023-11-23 15:25:47 +0530

    First Commit

--- DemoWebShop/src/main/java/com/ff/genericUtility/ReadExceFile.java
+++
@@ -1,5 +0,0 @@
-package com.ff.genericUtility;
-
-public class ReadExceFile {
-
-}
---
+++ Sample/src/Samp1/TC1.java
@@ -0,0 +1,8 @@
+package Samp1;
+
+public class TC1 {
+	public static void main(String[] args) {
+		System.out.println("HI world");
+	}
+
+}
